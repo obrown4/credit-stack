38356ec8f93f1e02560ac3e5e4b3d5df18e73abe
Owen Brown <ombrown@umich.edu>
2025-02-08 13:28:44 -0500

    Folder structure

--- src/background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    console.log("Tab updated:", tab.url);
-
-    // Ensure the page is fully loaded and avoid restricted pages
-    if (changeInfo.status === 'complete') {
-        const result = await chrome.tabs.sendMessage(tabId, { type: "NEW_PAGE" });
-    }
-});
